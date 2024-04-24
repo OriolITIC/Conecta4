3ae10ec885314e92233f63b088eac8644809b53f
lMrSl <apalenqu22@jaumebalmes.net>
2024-04-24 19:09:25 +0200

    sebas

--- app/src/androidTest/java/com/example/connect4/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/connect4/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.conecta4;
+package com.example.connect4;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.conecta4", appContext.getPackageName());
+        assertEquals("com.example.connect4", appContext.getPackageName());
     }
 }
